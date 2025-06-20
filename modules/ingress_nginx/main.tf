f082cbc20e48bde1d3ba56a2c9ab0d91413a4879
Jonathan Haab <jonathan.haab@chuv.ch>
2025-06-20 16:22:14 +0200

    remove obselete output

--- modules/ingress_nginx/main.tf
+++ modules/ingress_nginx/main.tf
@@ -62,7 +62,3 @@ output "loadbalancer_ip" {
   value = data.kubernetes_service.loadbalancer.status.0.load_balancer.0.ingress.0.ip
   depends_on = [ null_resource.wait_for_lb_ip ]
 }
-
-output "ingress_nginx_namespace" {
-  value = local.ingress_nginx_namespace
-}
