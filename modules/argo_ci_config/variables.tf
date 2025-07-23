1b42a713a7fbc21602b8e75f2977e803f77dcb7d
Jonathan Haab <jonathan.haab@chuv.ch>
2025-07-23 15:33:39 +0200

    add github username, drop redundant argoci prefix

--- modules/argo_ci_config/variables.tf
+++ modules/argo_ci_config/variables.tf
@@ -8,28 +8,33 @@ variable "argoci_helm_values" {
   type        = string
 }
 
-variable "argoci_github_workbench_operator_token" {
+variable "github_workbench_operator_token" {
   description = "GitHub token for the Workbench Operator repository"
   type        = string
   sensitive   = true
 }
 
-variable "argoci_github_chorus_web_ui_token" {
+variable "github_chorus_web_ui_token" {
   description = "GitHub token for the Chorus Web UI repository"
   type        = string
   sensitive   = true
 }
 
-variable "argoci_github_images_token" {
+variable "github_images_token" {
   description = "GitHub token for the Images repository"
   type        = string
   sensitive   = true
 }
 
-variable "argoci_github_chorus_backend_token" {
+variable "github_chorus_backend_token" {
   description = "GitHub token for the Chorus Backend repository"
   type        = string
   sensitive   = true
+}
+
+variable "github_username" {
+  description = "GitHub username for Argo Workflows to use"
+  type        = string
 }
 
 variable "registry_server" {
