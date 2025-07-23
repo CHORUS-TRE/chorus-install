10400227258c83e169316d341aebd03634bb6b18
Jonathan Haab <jonathan.haab@chuv.ch>
2025-07-23 15:34:19 +0200

    add github username, drop redundant argoci prefix

--- stage_02/variables.tf
+++ stage_02/variables.tf
@@ -283,25 +283,30 @@ variable "grafana_admin_username" {
   default     = "admin"
 }
 
-variable "argoci_github_workbench_operator_token" {
+variable "github_username" {
+  description = "GitHub username owner of all the tokens"
+  type        = string
+}
+
+variable "github_workbench_operator_token" {
   description = "GitHub token for the Workbench Operator repository"
   type        = string
   sensitive   = true
 }
 
-variable "argoci_github_chorus_web_ui_token" {
+variable "github_chorus_web_ui_token" {
   description = "GitHub token for the Chorus Web UI repository"
   type        = string
   sensitive   = true
 }
 
-variable "argoci_github_images_token" {
+variable "github_images_token" {
   description = "GitHub token for the Images repository"
   type        = string
   sensitive   = true
 }
 
-variable "argoci_github_chorus_backend_token" {
+variable "github_chorus_backend_token" {
   description = "GitHub token for the Chorus Backend repository"
   type        = string
   sensitive   = true
