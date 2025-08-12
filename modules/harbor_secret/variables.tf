e8d5eb27c9a3f131437656c6fd2a9cc1a91463a8
Jonathan Haab <jonathan.haab@chuv.ch>
2025-08-12 16:58:27 +0200

    add harbor remote cluster secret WIP

---
+++ modules/harbor_secret/variables.tf
@@ -0,0 +1,74 @@
+variable "namespace" {
+  type        = string
+  description = "The Kubernetes namespace where Harbor is deployed"
+}
+
+variable "secret_name" {
+  type        = string
+  description = "The name of the Kubernetes Secret that contains shared Harbor secrets (e.g., core, token, etc.)"
+}
+
+variable "secret_key_secret_name" {
+  type        = string
+  description = "The name of the Kubernetes Secret that contains the encryption secret key"
+}
+
+variable "xsrf_secret_name" {
+  type        = string
+  description = "The name of the Kubernetes Secret containing the XSRF protection secret for Harbor"
+}
+
+variable "xsrf_secret_key" {
+  type        = string
+  description = "The key in the XSRF protection secret that stores the actual token"
+}
+
+variable "admin_password_secret_name" {
+  type        = string
+  description = "The name of the Kubernetes Secret that contains the Harbor admin password"
+}
+
+variable "admin_password_secret_key" {
+  type        = string
+  description = "The key in the Harbor admin password secret that stores the actual admin password"
+}
+
+variable "jobservice_secret_name" {
+  type        = string
+  description = "The name of the Kubernetes Secret containing the jobservice secret for Harbor's internal communication"
+}
+
+variable "jobservice_secret_key" {
+  type        = string
+  description = "The key in the jobservice secret that stores the actual jobservice secret"
+}
+
+variable "registry_secret_name" {
+  type        = string
+  description = "The name of the Kubernetes Secret containing the HTTP secret used by the Harbor registry"
+}
+
+variable "registry_secret_key" {
+  type        = string
+  description = "The key within the Harbor registry HTTP secret that holds the shared HTTP secret"
+}
+
+variable "registry_credentials_secret_name" {
+  type        = string
+  description = "The name of the Kubernetes Secret containing Harbor registry credentials"
+}
+
+variable "oidc_secret_name" {
+  type        = string
+  description = "The name of the Kubernetes Secret containing the OIDC secret for Harbor authentication"
+}
+
+variable "oidc_secret_key" {
+  type        = string
+  description = "The key in the OIDC secret that contains the OIDC client secret for Harbor authentication"
+}
+
+variable "oidc_config" {
+  type        = string
+  description = "A string containing the OIDC configuration in JSON format for Harbor integration"
+}
--- modules/remote_cluster/main.tf
+++ modules/remote_cluster/main.tf
@@ -9,6 +9,42 @@ locals {
   keycloak_db_existing_secret    = "keycloak_db_secret"
   keycloak_db_admin_password_key = "postgres-password"
   keycloak_db_user_password_key  = "password"
+
+  harbor_namespace             = "harbor"
+  harbor_existing_secret       = "harbor-secret"
+  harbor_password_secret_key   = "adminPassword"
+  harbor_db_existing_secret    = "harbor_db_secret"
+  harbor_db_admin_password_key = "postgres-password"
+  harbor_db_user_password_key  = "password"
+
+  harbor_existing_secret_secret_key           = "harbor-key"
+  harbor_existing_xsrf_secret                 = "harbor-xsrf"
+  harbor_existing_xsrf_secret_key             = "CSRF_KEY"
+  harbor_existing_admin_password              = "harbor-admin-password"
+  harbor_existing_secret_admin_password_key   = "HARBOR_ADMIN_PASSWORD"
+  harbor_existing_jobservice_secret           = "harbor-jobservice"
+  harbor_existing_jobservice_secret_key       = "JOBSERVICE_SECRET"
+  harbor_existing_registry_http_secret        = "harbor-registry"
+  harbor_existing_registry_http_secret_key    = "REGISTRY_HTTP_SECRET"
+  harbor_existing_registry_credentials_secret = "harbor-registry-credentials"
+  harbor_oidc_secret                          = "harbor-oidc"
+  harbor_oidc_secret_key                      = "CONFIG_OVERWRITE_JSON"
+  harbor_oidc_config                          = <<EOT
+  {
+  "auth_mode": "oidc_auth",
+  "primary_auth_mode": "true",
+  "oidc_name": "Keycloak",
+  "oidc_endpoint": "todo",
+  "oidc_client_id": "todo",
+  "oidc_client_secret": "todo",
+  "oidc_groups_claim": "groups",
+  "oidc_admin_group": "todo",
+  "oidc_scope": "openid,profile,offline_access,email,groups",
+  "oidc_verify_cert": "false",
+  "oidc_auto_onboard": "true",
+  "oidc_user_claim": "name"
+  }
+  EOT
 }
 
 # Keycloak
@@ -47,6 +83,49 @@ module "keycloak_secret" {
 
 # Harbor
 
+import {
+  to = kubernetes_namespace.harbor
+  id = local.harbor_namespace
+}
+
+resource "kubernetes_namespace" "harbor" {
+  metadata {
+    name = local.harbor_namespace
+  }
+}
+
+module "harbor_db_secret" {
+  source = "../db_secret"
+
+  namespace             = local.harbor_namespace
+  secret_name           = local.harbor_db_existing_secret
+  db_user_password_key  = local.harbor_db_user_password_key
+  db_admin_password_key = local.harbor_db_admin_password_key
+
+  depends_on = [kubernetes_namespace.harbor]
+}
+
+module "harbor_secret" {
+  source = "../harbor_secret"
+
+  namespace                        = local.harbor_namespace
+  secret_name                      = local.harbor_existing_secret
+  secret_key_secret_name           = local.harbor_existing_secret_secret_key
+  xsrf_secret_name                 = local.harbor_existing_xsrf_secret
+  xsrf_secret_key                  = local.harbor_existing_xsrf_secret_key
+  admin_password_secret_name       = local.harbor_existing_admin_password
+  admin_password_secret_key        = local.harbor_existing_secret_admin_password_key
+  jobservice_secret_name           = local.harbor_existing_jobservice_secret
+  jobservice_secret_key            = local.harbor_existing_jobservice_secret_key
+  registry_secret_name             = local.harbor_existing_registry_http_secret
+  registry_secret_key              = local.harbor_existing_registry_http_secret_key
+  registry_credentials_secret_name = local.harbor_existing_registry_credentials_secret
+  oidc_secret_name                 = local.harbor_oidc_secret
+  oidc_secret_key                  = local.harbor_oidc_secret_key
+  oidc_config                      = local.harbor_oidc_config
+
+  depends_on = [kubernetes_namespace.harbor]
+}
 
 
 # need to upload the following charts
