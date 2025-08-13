7f595d0fb27d3cdc146279aab7e5547fbc6323a5
Jonathan Haab <jonathan.haab@chuv.ch>
2025-08-13 14:21:09 +0200

    test ns import

--- modules/ingress_nginx/main.tf
+++ modules/ingress_nginx/main.tf
@@ -1,5 +1,10 @@
 # Namespace
 
+import {
+  to = kubernetes_namespace.ingress_nginx
+  id = var.namespace
+}
+
 resource "kubernetes_namespace" "ingress_nginx" {
   metadata {
     name = var.namespace
