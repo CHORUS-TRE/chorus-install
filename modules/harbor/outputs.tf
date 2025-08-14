d53d8a782ddb6f3514759294432a50486a05972b
Jonathan Haab <jonathan.haab@chuv.ch>
2025-08-14 16:12:46 +0200

    fix outputs

--- modules/harbor/outputs.tf
+++ modules/harbor/outputs.tf
@@ -10,13 +10,13 @@ output "harbor_password" {
 }
 
 output "harbor_db_password" {
-  value       = try(random_password.harbor_db_password.result, null)
+  value       = try(module.db_secret.db_password, null)
   description = "Harbor DB password for Harbor user"
   sensitive   = true
 }
 
 output "harbor_db_admin_password" {
-  value       = try(random_password.harbor_db_admin_password.result, null)
+  value       = try(module.db_secret.db_admin_password, null)
   description = "Habor DB password for Postgres user"
   sensitive   = true
 }
--- modules/keycloak_secret/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "keycloak_password" {
-  value       = data.kubernetes_secret.keycloak_admin_password.data["${local.keycloak_values_parsed.keycloak.auth.passwordSecretKey}"]
-  description = "Keycloak password"
-  sensitive   = true
-}
