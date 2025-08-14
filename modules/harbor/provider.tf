f463e8480d1fed4b0cf16f2581fa581bf008d38c
Jonathan Haab <jonathan.haab@chuv.ch>
2025-08-14 16:07:29 +0200

    fix providers

--- modules/harbor/provider.tf
+++ modules/harbor/provider.tf
@@ -8,14 +8,6 @@ terraform {
       source  = "hashicorp/helm"
       version = "2.17.0"
     }
-    random = {
-      source  = "registry.terraform.io/hashicorp/random"
-      version = "3.7.2"
-    }
-    htpasswd = {
-      source  = "loafoe/htpasswd"
-      version = "1.2.1"
-    }
   }
   # Provider functions require Terraform 1.8 and later.
   required_version = ">= 1.8.0"
