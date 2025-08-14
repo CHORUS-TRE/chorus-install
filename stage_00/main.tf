438de009689eca7c34f17c4f466e1a6f8b0066a7
Jonathan Haab <jonathan.haab@chuv.ch>
2025-08-14 10:35:05 +0200

    remove obselete local-exec block

--- stage_00/main.tf
+++ stage_00/main.tf
@@ -40,27 +40,6 @@ data "external" "cert_manager_config" {
   program    = ["/bin/cat", "${var.helm_values_path}/${var.cluster_name}/${var.cert_manager_chart_name}/config.json"]
 }
 
-resource "null_resource" "fetch_cert_manager_app_version" {
-  provisioner "local-exec" {
-    quiet       = true
-    command     = <<EOT
-      set -e
-      mkdir -p ${path.module}/tmp
-      helm registry login ${var.helm_registry} -u ${var.helm_registry_username} -p ${var.helm_registry_password} >/dev/null
-      helm pull "oci://${var.helm_registry}/charts/${var.cert_manager_chart_name}" --version ${local.cert_manager_chart_version} --destination ${path.module}/tmp
-      tar -xzf ${path.module}/tmp/cert-manager-*.tgz -C ${path.module}/tmp
-      touch ${var.helm_values_path}/${var.cluster_name}/${var.cert_manager_chart_name}/app_version
-      printf '%s' $(yq '.dependencies[0].version' ${path.module}/tmp/${var.cert_manager_chart_name}/Chart.yaml) > ${var.helm_values_path}/${var.cluster_name}/${var.cert_manager_chart_name}/app_version
-      rm -r ${path.module}/tmp
-    EOT
-    interpreter = ["/bin/sh", "-c"]
-  }
-
-  triggers = {
-    always_run = timestamp()
-  }
-}
-
 data "external" "cert_manager_app_version" {
   program = [
     "/bin/sh", "-c",
