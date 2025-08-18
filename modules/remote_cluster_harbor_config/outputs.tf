7d80e03ba1ad35b47200926131c3451337ad9d5e
Jonathan Haab <jonathan.haab@chuv.ch>
2025-08-18 15:24:48 +0200

    fix output

--- modules/remote_cluster_harbor_config/outputs.tf
+++ modules/remote_cluster_harbor_config/outputs.tf
@@ -1,23 +1,11 @@
-output "github_actions_robot_password" {
-  value       = try(random_password.github_actions_robot_password.result, null)
-  description = "Password of the robot user used by GitHub Actions"
+output "cluster_robot_password" {
+  value       = try(random_password.cluster_robot_password, null)
+  description = "Password of the robot user used by the cluster's components"
   sensitive   = true
 }
 
-output "argocd_robot_password" {
-  value       = try(random_password.argocd_robot_password.result, null)
-  description = "Password of the robot user used by ArgoCD"
-  sensitive   = true
-}
-
-output "chorusci_robot_password" {
-  value       = try(random_password.chorusci_robot_password.result, null)
-  description = "Password of the robot user used by ChorusCI"
-  sensitive   = true
-}
-
-output "renovate_robot_password" {
-  value       = try(random_password.renovate_robot_password.result, null)
-  description = "Password of the robot user used by Renovate"
+output "build_robot_password" {
+  value       = try(random_password.build_robot_password.result, null)
+  description = "Password of the robot user used by the Chorus build cluster"
   sensitive   = true
 }
