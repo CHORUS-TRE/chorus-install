5845049d6d87a26ce6c5b25e4d8ed893793aa65e
Jonathan Haab <jonathan.haab@chuv.ch>
2025-08-19 18:01:49 +0200

    fix output

--- modules/remote_cluster_harbor_config/outputs.tf
+++ modules/remote_cluster_harbor_config/outputs.tf
@@ -1,5 +1,5 @@
 output "cluster_robot_password" {
-  value       = try(random_password.cluster_robot_password, null)
+  value       = try(random_password.cluster_robot_password.result, null)
   description = "Password of the robot user used by the cluster's components"
   sensitive   = true
 }
