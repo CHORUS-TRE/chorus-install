b8c0d4602b9661762903a1ec7989a1066f721d0e
Jonathan Haab <jonathan.haab@chuv.ch>
2025-08-19 11:53:22 +0200

    add auth flow

--- modules/keycloak_backend_client_config/main.tf
+++ modules/keycloak_backend_client_config/main.tf
@@ -1,14 +1,40 @@
-module "keycloak_generic_client_config" {
-  source = "../keycloak_generic_client_config"
+resource "keycloak_openid_client" "openid_client" {
+  realm_id      = var.realm_id
+  client_id     = var.client_id
+  name          = title(var.client_id)
+  client_secret = var.client_secret
+  enabled       = true
+  access_type   = "CONFIDENTIAL"
 
-  realm_id            = var.realm_id
-  client_id           = var.client_id
-  client_secret       = var.client_secret
   root_url            = var.root_url
   base_url            = var.base_url
   admin_url           = var.admin_url
   web_origins         = var.web_origins
   valid_redirect_uris = var.valid_redirect_uris
+
+  standard_flow_enabled        = true
+  implicit_flow_enabled        = true
+  direct_access_grants_enabled = true
+  frontchannel_logout_enabled  = true
+  oauth2_device_authorization_grant_enabled = true
+  # TODO: add oidc_ciba_grant_enablesd = true
+  # option does not seem to be available yet
+}
+
+data "keycloak_openid_client" "openid_cliend" {
+  realm_id   = var.realm_id
+  client_id  = var.client_id
+  depends_on = [keycloak_openid_client.openid_client]
+}
+
+resource "keycloak_openid_client_optional_scopes" "client_optional_scopes" {
+  realm_id  = var.realm_id
+  client_id = data.keycloak_openid_client.openid_cliend.id
+
+  optional_scopes = [
+    "offline_access",
+    "groups"
+  ]
 }
 
 resource "keycloak_group" "backend_uma_protection" {
