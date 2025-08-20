b675d03380821204a90e216bf3df7ca7b9d85c94
Jonathan Haab <jonathan.haab@chuv.ch>
2025-08-20 09:09:02 +0200

    use remote cluster name as backend realm name

--- stage_04/main.tf
+++ stage_04/main.tf
@@ -188,7 +188,7 @@ module "remote_cluster_keycloak_config" {
 
   admin_id           = var.keycloak_admin_username
   infra_realm_name   = var.keycloak_infra_realm
-  backend_realm_name = var.keycloak_backend_realm
+  backend_realm_name = local.remote_cluster_name
 }
 
 module "keycloak_harbor_client_config" {
