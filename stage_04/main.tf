76ef5588de0c4df3ee49d86cd27648f7dff84638
Jonathan Haab <jonathan.haab@chuv.ch>
2025-08-20 14:34:35 +0200

    return to using the existing secret for matomo db

--- stage_04/main.tf
+++ stage_04/main.tf
@@ -439,26 +439,38 @@ resource "kubernetes_secret" "matomo_secret" {
   }
 }
 
-# Fetch the mariadb secret which is generated automatically
-
-data "kubernetes_secret" "mariadb_secret" {
-  metadata {
-    name = "mariadb-secret"
-    namespace = local.matomo_db_namespace
-  }
+resource "random_password" "mariadb_password" {
+  length  = 32
+  special = true
 }
 
-# Make the mariadb password available to matomo
+resource "random_password" "mariadb_replication_password" {
+  length  = 32
+  special = true
+}
 
-resource "kubernetes_secret" "matomo_db_secret" {
+resource "random_password" "mariadb_root_password" {
+  length  = 32
+  special = true
+}
+
+# TODO: Currently, the matomo chart looks for 
+# the key "db-password". Let's check if this 
+# can be changed so that we don't have to duplicate
+# the mariadb password twice
+resource "kubernetes_secret" "mariadb_secret" {
 
   metadata {
     name      = local.matomo_db_secret_name
     namespace = local.matomo_db_namespace
+
   }
 
   data = {
-    db-password = data.kubernetes_secret.mariadb_secret.data.madiadb_password
+    db-password = random_password.mariadb_password.result
+    mariadb-password = random_password.mariadb_password.result
+    mariadb-replication-password = random_password.mariadb_replication_password.result
+    mariadb-root-password = random_password.mariadb_root_password.result
   }
 }
 
