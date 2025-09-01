74a3d83d9533727219aab8ff1c1a86ca84af3501
Jonathan Haab <jonathan.haab@chuv.ch>
2025-09-01 15:29:50 +0200

    refactor cluster_name var

--- stage_01/main.tf
+++ stage_01/main.tf
@@ -1,33 +1,34 @@
 locals {
-  ingress_nginx_chart_version = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.ingress_nginx_chart_name}/config.json")).version
-  cert_manager_chart_version  = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.cert_manager_chart_name}/config.json")).version
-  selfsigned_chart_version    = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.selfsigned_chart_name}/config.json")).version
-  keycloak_chart_version      = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.keycloak_chart_name}/config.json")).version
-  keycloak_db_chart_version   = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.keycloak_chart_name}-db/config.json")).version
-  harbor_chart_version        = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.harbor_chart_name}/config.json")).version
-  harbor_cache_chart_version  = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.harbor_chart_name}-cache/config.json")).version
-  harbor_db_chart_version     = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.harbor_chart_name}-db/config.json")).version
+  cluster_name                = coalesce(var.cluster_name, var.kubeconfig_context)
+  ingress_nginx_chart_version = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.ingress_nginx_chart_name}/config.json")).version
+  cert_manager_chart_version  = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.cert_manager_chart_name}/config.json")).version
+  selfsigned_chart_version    = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.selfsigned_chart_name}/config.json")).version
+  keycloak_chart_version      = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.keycloak_chart_name}/config.json")).version
+  keycloak_db_chart_version   = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.keycloak_chart_name}-db/config.json")).version
+  harbor_chart_version        = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.harbor_chart_name}/config.json")).version
+  harbor_cache_chart_version  = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.harbor_chart_name}-cache/config.json")).version
+  harbor_db_chart_version     = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.harbor_chart_name}-db/config.json")).version
 
-  ingress_nginx_namespace = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.ingress_nginx_chart_name}/config.json")).namespace
-  cert_manager_namespace  = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.cert_manager_chart_name}/config.json")).namespace
-  keycloak_namespace      = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.keycloak_chart_name}/config.json")).namespace
-  harbor_namespace        = jsondecode(file("${var.helm_values_path}/${var.cluster_name}/${var.harbor_chart_name}/config.json")).namespace
+  ingress_nginx_namespace = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.ingress_nginx_chart_name}/config.json")).namespace
+  cert_manager_namespace  = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.cert_manager_chart_name}/config.json")).namespace
+  keycloak_namespace      = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.keycloak_chart_name}/config.json")).namespace
+  harbor_namespace        = jsondecode(file("${var.helm_values_path}/${local.cluster_name}/${var.harbor_chart_name}/config.json")).namespace
 
-  keycloak_helm_values   = file("${var.helm_values_path}/${var.cluster_name}/${var.keycloak_chart_name}/values.yaml")
+  keycloak_helm_values   = file("${var.helm_values_path}/${local.cluster_name}/${var.keycloak_chart_name}/values.yaml")
   keycloak_values_parsed = yamldecode(local.keycloak_helm_values)
   keycloak_secret_name   = local.keycloak_values_parsed.keycloak.auth.existingSecret
   keycloak_secret_key    = local.keycloak_values_parsed.keycloak.auth.passwordSecretKey
   keycloak_url           = "https://${local.keycloak_values_parsed.keycloak.ingress.hostname}"
 
-  keycloak_db_helm_values      = file("${var.helm_values_path}/${var.cluster_name}/${var.keycloak_chart_name}-db/values.yaml")
+  keycloak_db_helm_values      = file("${var.helm_values_path}/${local.cluster_name}/${var.keycloak_chart_name}-db/values.yaml")
   keycloak_db_values_parsed    = yamldecode(local.keycloak_db_helm_values)
   keycloak_db_secret_name      = local.keycloak_db_values_parsed.postgresql.global.postgresql.auth.existingSecret
   keycloak_db_admin_secret_key = local.keycloak_db_values_parsed.postgresql.global.postgresql.auth.secretKeys.adminPasswordKey
   keycloak_db_user_secret_key  = local.keycloak_db_values_parsed.postgresql.global.postgresql.auth.secretKeys.userPasswordKey
 
-  harbor_helm_values       = file("${var.helm_values_path}/${var.cluster_name}/${var.harbor_chart_name}/values.yaml")
-  harbor_cache_helm_values = file("${var.helm_values_path}/${var.cluster_name}/${var.harbor_chart_name}-cache/values.yaml")
-  harbor_db_helm_values    = file("${var.helm_values_path}/${var.cluster_name}/${var.harbor_chart_name}-db/values.yaml")
+  harbor_helm_values       = file("${var.helm_values_path}/${local.cluster_name}/${var.harbor_chart_name}/values.yaml")
+  harbor_cache_helm_values = file("${var.helm_values_path}/${local.cluster_name}/${var.harbor_chart_name}-cache/values.yaml")
+  harbor_db_helm_values    = file("${var.helm_values_path}/${local.cluster_name}/${var.harbor_chart_name}-db/values.yaml")
 
   harbor_db_values_parsed    = yamldecode(local.harbor_db_helm_values)
   harbor_db_secret_name      = local.harbor_db_values_parsed.postgresql.global.postgresql.auth.existingSecret
@@ -64,7 +65,7 @@ locals {
       oidc_client_secret = random_password.harbor_keycloak_client_secret.result
       oidc_admin_group   = var.harbor_keycloak_oidc_admin_group
     }
-  ))   #TODO: set oidc_verify_cert to "true"
+  )) #TODO: set oidc_verify_cert to "true"
 }
 
 # Install charts
@@ -91,12 +92,12 @@ module "ingress_nginx" {
     helm = helm.chorus_helm
   }
 
-  cluster_name  = var.cluster_name
+  cluster_name  = local.cluster_name
   helm_registry = var.helm_registry
 
   chart_name         = var.ingress_nginx_chart_name
   chart_version      = local.ingress_nginx_chart_version
-  helm_values        = file("${var.helm_values_path}/${var.cluster_name}/${var.ingress_nginx_chart_name}/values.yaml")
+  helm_values        = file("${var.helm_values_path}/${local.cluster_name}/${var.ingress_nginx_chart_name}/values.yaml")
   namespace          = local.ingress_nginx_namespace
   kubeconfig_path    = var.kubeconfig_path
   kubeconfig_context = var.kubeconfig_context
@@ -109,18 +110,18 @@ module "certificate_authorities" {
     helm = helm.chorus_helm
   }
 
-  cluster_name  = var.cluster_name
+  cluster_name  = local.cluster_name
   helm_registry = var.helm_registry
 
   cert_manager_chart_name    = var.cert_manager_chart_name
   cert_manager_chart_version = local.cert_manager_chart_version
-  cert_manager_helm_values   = file("${var.helm_values_path}/${var.cluster_name}/${var.cert_manager_chart_name}/values.yaml")
+  cert_manager_helm_values   = file("${var.helm_values_path}/${local.cluster_name}/${var.cert_manager_chart_name}/values.yaml")
   cert_manager_namespace     = local.cert_manager_namespace
   cert_manager_crds_path     = var.cert_manager_crds_path
 
   selfsigned_chart_name    = var.selfsigned_chart_name
   selfsigned_chart_version = local.selfsigned_chart_version
-  selfsigned_helm_values   = file("${var.helm_values_path}/${var.cluster_name}/${var.selfsigned_chart_name}/values.yaml")
+  selfsigned_helm_values   = file("${var.helm_values_path}/${local.cluster_name}/${var.selfsigned_chart_name}/values.yaml")
 
   kubeconfig_path    = var.kubeconfig_path
   kubeconfig_context = var.kubeconfig_context
@@ -133,7 +134,7 @@ module "keycloak" {
     helm = helm.chorus_helm
   }
 
-  cluster_name  = var.cluster_name
+  cluster_name  = local.cluster_name
   helm_registry = var.helm_registry
 
   keycloak_chart_name    = var.keycloak_chart_name
@@ -169,7 +170,7 @@ resource "random_password" "argocd_keycloak_client_secret" {
 module "harbor" {
   source = "../modules/harbor"
 
-  cluster_name  = var.cluster_name
+  cluster_name  = local.cluster_name
   helm_registry = var.helm_registry
 
   harbor_chart_name     = var.harbor_chart_name
