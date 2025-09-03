3951e57e9782cac296f21fd43fff06f97d9dace1
Jonathan Haab <jonathan.haab@chuv.ch>
2025-09-03 14:30:38 +0200

    move duplicated code into dedicated module

--- modules/keycloak_config/main.tf
+++ modules/keycloak_config/main.tf
@@ -19,18 +19,15 @@ resource "keycloak_group_roles" "chorus_admins_group_roles" {
   role_ids = [data.keycloak_role.admin.id]
 }
 
-resource "keycloak_realm" "infra" {
-  realm                       = var.infra_realm_name
-  default_signature_algorithm = "RS256"
-  revoke_refresh_token        = true
-  refresh_token_max_reuse     = 0
-  password_policy          = "length(8) and specialChars(1) and upperCase(1) and lowerCase(1) and digits(1) and notUsername and notEmail and notContainsUsername"
+module "keycloak_realm" {
+  source = "../keycloak_realm"
+  realm_name = var.infra_realm_name
 }
 
 # Client scope
 
 resource "keycloak_openid_client_scope" "groups_client_scope" {
-  realm_id               = keycloak_realm.infra.id
+  realm_id               = module.keycloak_realm.realm_id
   name                   = "groups"
   description            = "When requested, this scope will map a user's group memberships to a claim"
   include_in_token_scope = true
@@ -39,112 +36,10 @@ resource "keycloak_openid_client_scope" "groups_client_scope" {
 # Group membership mapper
 
 resource "keycloak_openid_group_membership_protocol_mapper" "group_membership_mapper" {
-  realm_id        = keycloak_realm.infra.id
+  realm_id        = module.keycloak_realm.realm_id
   client_scope_id = keycloak_openid_client_scope.groups_client_scope.id
   name            = "groups"
 
   claim_name = "groups"
   full_path  = false
-}
-
-# User profile
-
-resource "keycloak_realm_user_profile" "userprofile" {
-  realm_id = keycloak_realm.infra.id
-
-  attribute {
-    name         = "username"
-    display_name = "$${username}"
-
-    permissions {
-      view = ["admin", "user"]
-      edit = ["admin", "user"]
-    }
-
-    validator {
-      name = "length"
-      config = {
-        min = 3
-        max = 8
-      }
-    }
-
-    validator {
-      name = "pattern"
-      config = {
-        pattern       = "^(?!(root|bin|daemon|adm|lp|sync|shutdown|halt|mail|news|uucp|operator|games|gopher|nobody|www-data|proxy|backup|sys|man|postfix|sshd|ftp|mysql|postgres|dnsmasq|ntp|messagebus|haldaemon|rpc|avahi|saned|usbmux|kernoops|admin|sysadmin|user|guest|test|administrator|rootuser|default|system|superuser)$)[a-z_][a-z0-9_-]*$"
-        error-message = "Invalid username"
-      }
-    }
-  }
-
-  attribute {
-    name         = "email"
-    display_name = "$${email}"
-
-    required_for_roles = ["user"]
-
-    permissions {
-      view = ["admin", "user"]
-      edit = ["admin", "user"]
-    }
-
-    validator {
-      name   = "email"
-      config = {}
-    }
-
-    validator {
-      name = "length"
-      config = {
-        max = 255
-      }
-    }
-  }
-
-  attribute {
-    name         = "firstName"
-    display_name = "$${firstName}"
-
-    required_for_roles = ["user"]
-
-    permissions {
-      view = ["admin", "user"]
-      edit = ["admin", "user"]
-    }
-
-    validator {
-      name = "length"
-      config = {
-        max = 255
-      }
-    }
-
-    validator {
-      name = "person-name-prohibited-characters"
-    }
-  }
-
-  attribute {
-    name         = "lastName"
-    display_name = "$${lastName}"
-
-    required_for_roles = ["user"]
-
-    permissions {
-      view = ["admin", "user"]
-      edit = ["admin", "user"]
-    }
-
-    validator {
-      name = "length"
-      config = {
-        max = 255
-      }
-    }
-
-    validator {
-      name = "person-name-prohibited-characters"
-    }
-  }
 }
---
+++ modules/keycloak_realm/main.tf
@@ -0,0 +1,112 @@
+resource "keycloak_realm" "new_realm" {
+  realm                       = var.realm_name
+  default_signature_algorithm = "RS256"
+  revoke_refresh_token        = true
+  refresh_token_max_reuse     = 0
+
+  registration_allowed     = true
+  login_with_email_allowed = true
+  password_policy          = "length(8) and specialChars(1) and upperCase(1) and lowerCase(1) and digits(1) and notUsername and notEmail and notContainsUsername"
+}
+
+# User profile
+
+resource "keycloak_realm_user_profile" "userprofile" {
+  realm_id = keycloak_realm.new_realm.id
+
+  attribute {
+    name         = "username"
+    display_name = "$${username}"
+
+    permissions {
+      view = ["admin", "user"]
+      edit = ["admin", "user"]
+    }
+
+    validator {
+      name = "length"
+      config = {
+        min = 3
+        max = 8
+      }
+    }
+
+    validator {
+      name = "pattern"
+      config = {
+        pattern       = "^(?!(root|bin|daemon|adm|lp|sync|shutdown|halt|mail|news|uucp|operator|games|gopher|nobody|www-data|proxy|backup|sys|man|postfix|sshd|ftp|mysql|postgres|dnsmasq|ntp|messagebus|haldaemon|rpc|avahi|saned|usbmux|kernoops|admin|sysadmin|user|guest|test|administrator|rootuser|default|system|superuser)$)[a-z_][a-z0-9_-]*$"
+        error-message = "Invalid username"
+      }
+    }
+  }
+
+  attribute {
+    name         = "email"
+    display_name = "$${email}"
+
+    required_for_roles = ["user"]
+
+    permissions {
+      view = ["admin", "user"]
+      edit = ["admin", "user"]
+    }
+
+    validator {
+      name   = "email"
+      config = {}
+    }
+
+    validator {
+      name = "length"
+      config = {
+        max = 255
+      }
+    }
+  }
+
+  attribute {
+    name         = "firstName"
+    display_name = "$${firstName}"
+
+    required_for_roles = ["user"]
+
+    permissions {
+      view = ["admin", "user"]
+      edit = ["admin", "user"]
+    }
+
+    validator {
+      name = "length"
+      config = {
+        max = 255
+      }
+    }
+
+    validator {
+      name = "person-name-prohibited-characters"
+    }
+  }
+
+  attribute {
+    name         = "lastName"
+    display_name = "$${lastName}"
+
+    required_for_roles = ["user"]
+
+    permissions {
+      view = ["admin", "user"]
+      edit = ["admin", "user"]
+    }
+
+    validator {
+      name = "length"
+      config = {
+        max = 255
+      }
+    }
+
+    validator {
+      name = "person-name-prohibited-characters"
+    }
+  }
+}
