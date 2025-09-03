4d22b3b4cf85adf731c972dcaa64b877a5b624a9
Jonathan Haab <jonathan.haab@chuv.ch>
2025-09-03 11:15:15 +0200

    add user profile username validations

--- modules/remote_cluster_keycloak_config/main.tf
+++ modules/remote_cluster_keycloak_config/main.tf
@@ -14,3 +14,34 @@ resource "keycloak_realm" "backend" {
   registration_allowed     = true
   login_with_email_allowed = true
 }
+
+# User profile
+
+resource "keycloak_realm_user_profile" "userprofile" {
+  realm_id = keycloak_realm.backend.id
+
+  attribute {
+    name = "username"
+
+    validator {
+      name = "length"
+      config = {
+        min = 3
+        max = 8
+      }
+    }
+
+    validator {
+      name = "pattern"
+      config = {
+        pattern = "^(?!(root|bin|daemon|adm|lp|sync|shutdown|halt|mail|news|uucp|operator|games|gopher|nobody|www-data|proxy|backup|sys|man|postfix|sshd|ftp|mysql|postgres|dnsmasq|ntp|messagebus|haldaemon|rpc|avahi|saned|usbmux|kernoops|admin|sysadmin|user|guest|test|administrator|rootuser|default|system|superuser)$)[a-z_][a-z0-9_-]*$"
+        error-message = "Invalid username"
+      }
+    }
+  }
+}
+
+
+# Password policy
+
+# TODO
