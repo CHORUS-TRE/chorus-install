22c2920844fd9ab82e775cd9f141484c7c052cd1
Jonathan Haab <jonathan.haab@chuv.ch>
2025-09-18 14:39:24 +0200

    add identity provider for build cluster

--- modules/keycloak_config/variables.tf
+++ modules/keycloak_config/variables.tf
@@ -7,3 +7,16 @@ variable "infra_realm_name" {
   description = "Keycloak infrastructure realm name"
   type        = string
 }
+
+variable "google_identity_provider_client_id" {
+  description = "The Google client identifier"
+  type        = string
+  default     = ""
+}
+
+variable "google_identity_provider_client_secret" {
+  description = "The Google client secret used for authentication"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
