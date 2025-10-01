b8870668090b6268647f2f96e2494db907dd643f
Jonathan Haab <jonathan.haab@chuv.ch>
2025-10-01 13:57:51 +0200

    fix conditional creation

--- modules/remote_cluster_harbor_config/outputs.tf
+++ modules/remote_cluster_harbor_config/outputs.tf
@@ -5,7 +5,7 @@ output "cluster_robot_password" {
 }
 
 output "build_robot_password" {
-  value       = try(random_password.build_robot_password.result, null)
+  value       = try(random_password.build_robot_password[0].result, null)
   description = "Password of the robot user used by the Chorus build cluster"
   sensitive   = true
 }
