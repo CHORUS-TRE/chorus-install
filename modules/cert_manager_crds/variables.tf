a5be5fc92e4c89d7fa34ce12ac19e5e4453853a6
Jonathan Haab <jonathan.haab@chuv.ch>
2025-10-08 15:36:02 +0200

    remove redundant validation

--- modules/cert_manager_crds/variables.tf
+++ modules/cert_manager_crds/variables.tf
@@ -3,11 +3,6 @@ variable "cert_manager_crds_content" {
   description = "YAML content of the Cert-Manager CRDs to be applied. Should contain one or more Kubernetes manifests in YAML format."
 
   validation {
-    condition     = length(var.cert_manager_crds_content) > 0
-    error_message = "cert_manager_crds_content cannot be empty."
-  }
-
-  validation {
     condition     = can(yamldecode(var.cert_manager_crds_content))
     error_message = "cert_manager_crds_content must be valid YAML."
   }
