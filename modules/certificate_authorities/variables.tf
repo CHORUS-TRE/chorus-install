ee090079290a7a57f2fbbc8160d11fabaf68ba41
Jonathan Haab <jonathan.haab@chuv.ch>
2025-10-08 14:12:30 +0000

    remove validation

--- modules/certificate_authorities/variables.tf
+++ modules/certificate_authorities/variables.tf
@@ -127,8 +127,12 @@ variable "cert_manager_crds_content" {
   type        = string
   description = "YAML content of the Cert-Manager CRDs to be applied. Should contain one or more Kubernetes manifests in YAML format."
 
+/*
+  This validation is commented out because it throwns error
+  when feeding in the cert-manager crds file
   validation {
     condition     = can(yamldecode(var.cert_manager_crds_content))
     error_message = "cert_manager_crds_content must be valid YAML."
   }
+*/
 }
