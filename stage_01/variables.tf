881823c49785f4abf1ea38e2ffd906021d66059e
Jonathan Haab <jonathan.haab@chuv.ch>
2026-01-27 16:46:43 +0100

    tfsort

--- stage_01/variables.tf
+++ stage_01/variables.tf
@@ -1,156 +1,7 @@
-/*
-Do not add Kubernetes/Helm related values in this file.
-Instead, build upon the environment-template repository
-https://github.com/CHORUS-TRE/environment-template
-and reference it using the "helm_values_path" variable below
-*/
-
-variable "cluster_name" {
-  description = "The cluster name to be used as a prefix to release names"
+variable "alertmanager_keycloak_client_id" {
+  description = "Keycloak client ID used assigned to Alertmanager"
   type        = string
-}
-
-variable "kubeconfig_path" {
-  description = "Path to the Kubernetes config file"
-  type        = string
-}
-
-variable "kubeconfig_context" {
-  description = "Kubernetes context to use"
-  type        = string
-}
-
-variable "helm_registry" {
-  description = "CHORUS Helm chart registry"
-  type        = string
-}
-
-variable "helm_registry_username" {
-  description = "Username to connect to the CHORUS Helm chart registry"
-  type        = string
-  default     = ""
-}
-
-variable "helm_registry_password" {
-  description = "Password to connect to the CHORUS Helm chart registry"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "helm_values_path" {
-  description = "Path to the repository storing the Helm chart values"
-  type        = string
-  default     = "../values"
-}
-
-variable "ingress_nginx_chart_name" {
-  description = "Ingress-Nginx Helm chart folder name"
-  type        = string
-  default     = "ingress-nginx"
-}
-
-variable "cert_manager_chart_name" {
-  description = "Cert-Manager Helm chart folder name"
-  type        = string
-  default     = "cert-manager"
-}
-
-variable "selfsigned_chart_name" {
-  description = "Self-Signed Issuer Helm chart folder name"
-  type        = string
-  default     = "self-signed-issuer"
-}
-
-variable "cert_manager_crds_folder_name" {
-  description = "Name of the folder where the Cert-Manager CRDs file will be downloaded (pattern: folder_name/cluster_name/file_name)"
-  type        = string
-  default     = "../crds"
-}
-
-variable "cert_manager_crds_file_name" {
-  description = "Cert-Manager CRDs file name"
-  type        = string
-  default     = "cert-manager.crds.yaml"
-}
-
-variable "valkey_chart_name" {
-  description = "Valkey Helm chart folder name"
-  type        = string
-  default     = "valkey"
-}
-
-variable "keycloak_chart_name" {
-  description = "Keycloak Helm chart folder name"
-  type        = string
-  default     = "keycloak"
-}
-
-variable "postgresql_chart_name" {
-  description = "PostgreSQL Helm chart folder name"
-  type        = string
-  default     = "postgresql"
-}
-
-variable "harbor_chart_name" {
-  description = "Harbor Helm chart folder name"
-  type        = string
-  default     = "harbor"
-}
-
-variable "keycloak_realm" {
-  description = "Keycloak realm name"
-  type        = string
-  default     = "infra"
-}
-
-variable "harbor_keycloak_client_id" {
-  description = "Keycloak client ID assigned to Harbor"
-  type        = string
-  default     = "harbor"
-}
-
-variable "harbor_keycloak_oidc_admin_group" {
-  description = "Keycloak client ID assigned to Harbor"
-  type        = string
-  default     = "HarborAdmins"
-}
-
-variable "harbor_admin_username" {
-  description = "Harbor admin username"
-  type        = string
-  default     = "admin"
-}
-
-variable "grafana_admin_username" {
-  description = "Grafana admin username"
-  type        = string
-  default     = "admin"
-}
-
-variable "templates_path" {
-  description = "Path to the templates directory"
-  type        = string
-  default     = "../templates"
-}
-
-variable "chorus_priority_class_chart_name" {
-  description = "CHORUS Priority Class Helm chart folder name"
-  type        = string
-  default     = "chorus-priority-class"
-}
-
-variable "google_identity_provider_client_id" {
-  description = "The Google client identifier"
-  type        = string
-  default     = ""
-}
-
-variable "google_identity_provider_client_secret" {
-  description = "The Google client secret used for authentication"
-  type        = string
-  sensitive   = true
-  default     = ""
+  default     = "alertmanager"
 }
 
 variable "alertmanager_oauth2_proxy_chart_name" {
@@ -171,10 +22,52 @@ variable "argo_workflows_chart_name" {
   default     = "argo-workflows"
 }
 
+variable "argo_workflows_keycloak_client_id" {
+  description = "Keycloak client ID used assigned to Argo Workflows"
+  type        = string
+  default     = "argo-workflows"
+}
+
 variable "argocd_chart_name" {
   description = "ArgoCD Helm chart folder name"
   type        = string
   default     = "argo-cd"
+}
+
+variable "argocd_harbor_robot_username" {
+  description = "Harbor robot username used by ArgoCD"
+  type        = string
+  default     = "argo-cd"
+}
+
+variable "argocd_keycloak_client_id" {
+  description = "Keycloak client ID used assigned to ArgoCD"
+  type        = string
+  default     = "argocd"
+}
+
+variable "cert_manager_chart_name" {
+  description = "Cert-Manager Helm chart folder name"
+  type        = string
+  default     = "cert-manager"
+}
+
+variable "cert_manager_crds_file_name" {
+  description = "Cert-Manager CRDs file name"
+  type        = string
+  default     = "cert-manager.crds.yaml"
+}
+
+variable "cert_manager_crds_folder_name" {
+  description = "Name of the folder where the Cert-Manager CRDs file will be downloaded (pattern: folder_name/cluster_name/file_name)"
+  type        = string
+  default     = "../crds"
+}
+
+variable "chorus_priority_class_chart_name" {
+  description = "CHORUS Priority Class Helm chart folder name"
+  type        = string
+  default     = "chorus-priority-class"
 }
 
 variable "chorusci_chart_name" {
@@ -183,47 +76,33 @@ variable "chorusci_chart_name" {
   default     = "chorus-ci"
 }
 
-variable "kube_prometheus_stack_chart_name" {
-  description = "Kube Prometheus Stack Helm chart folder name"
+variable "cluster_name" {
+  description = "The cluster name to be used as a prefix to release names"
   type        = string
-  default     = "kube-prometheus-stack"
 }
 
-variable "oauth2_proxy_cache_chart_name" {
-  description = "OAuth2 Proxy Cache Helm chart folder name"
-  type        = string
-  default     = "oauth2-proxy-cache"
-}
-
-variable "prometheus_oauth2_proxy_chart_name" {
-  description = "Prometheus OAuth2 Proxy Helm chart folder name"
-  type        = string
-  default     = "prometheus-oauth2-proxy"
-}
-
-variable "webex_access_token" {
-  description = "The Webex access token for the Alertmanager integration"
+variable "github_chorus_backend_token" {
+  description = "GitHub token for the Chorus Backend repository"
   type        = string
   sensitive   = true
-  default     = ""
 }
 
-variable "alertmanager_keycloak_client_id" {
-  description = "Keycloak client ID used assigned to Alertmanager"
+variable "github_chorus_web_ui_token" {
+  description = "GitHub token for the Chorus Web UI repository"
   type        = string
-  default     = "alertmanager"
+  sensitive   = true
 }
 
-variable "prometheus_keycloak_client_id" {
-  description = "Keycloak client ID used assigned to Prometheus"
+variable "github_images_token" {
+  description = "GitHub token for the Images repository"
   type        = string
-  default     = "prometheus"
+  sensitive   = true
 }
 
-variable "argo_workflows_keycloak_client_id" {
-  description = "Keycloak client ID used assigned to Argo Workflows"
+variable "github_orga" {
+  description = "GitHub organization to use repositories from"
   type        = string
-  default     = "argo-workflows"
+  default     = "CHORUS-TRE"
 }
 
 variable "github_username" {
@@ -237,22 +116,65 @@ variable "github_workbench_operator_token" {
   sensitive   = true
 }
 
-variable "github_chorus_backend_token" {
-  description = "GitHub token for the Chorus Backend repository"
+variable "google_identity_provider_client_id" {
+  description = "The Google client identifier"
   type        = string
-  sensitive   = true
+  default     = ""
 }
 
-variable "github_images_token" {
-  description = "GitHub token for the Images repository"
+variable "google_identity_provider_client_secret" {
+  description = "The Google client secret used for authentication"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
-variable "github_chorus_web_ui_token" {
-  description = "GitHub token for the Chorus Web UI repository"
+variable "grafana_admin_username" {
+  description = "Grafana admin username"
+  type        = string
+  default     = "admin"
+}
+
+variable "harbor_admin_username" {
+  description = "Harbor admin username"
+  type        = string
+  default     = "admin"
+}
+
+variable "harbor_chart_name" {
+  description = "Harbor Helm chart folder name"
+  type        = string
+  default     = "harbor"
+}
+
+variable "harbor_keycloak_client_id" {
+  description = "Keycloak client ID assigned to Harbor"
+  type        = string
+  default     = "harbor"
+}
+
+variable "harbor_keycloak_oidc_admin_group" {
+  description = "Keycloak client ID assigned to Harbor"
+  type        = string
+  default     = "HarborAdmins"
+}
+
+variable "helm_registry" {
+  description = "CHORUS Helm chart registry"
+  type        = string
+}
+
+variable "helm_registry_password" {
+  description = "Password to connect to the CHORUS Helm chart registry"
   type        = string
   sensitive   = true
+  default     = ""
+}
+
+variable "helm_registry_username" {
+  description = "Username to connect to the CHORUS Helm chart registry"
+  type        = string
+  default     = ""
 }
 
 variable "helm_values_credentials_secret" {
@@ -268,26 +190,97 @@ variable "helm_values_pat" {
   default     = ""
 }
 
-variable "argocd_harbor_robot_username" {
-  description = "Harbor robot username used by ArgoCD"
+variable "helm_values_path" {
+  description = "Path to the repository storing the Helm chart values"
   type        = string
-  default     = "argo-cd"
-}
-
-variable "argocd_keycloak_client_id" {
-  description = "Keycloak client ID used assigned to ArgoCD"
-  type        = string
-  default     = "argocd"
-}
-
-variable "github_orga" {
-  description = "GitHub organization to use repositories from"
-  type        = string
-  default     = "CHORUS-TRE"
+  default     = "../values"
 }
 
 variable "helm_values_repo" {
   description = "GitHub repository to get the Helm charts values from"
   type        = string
   default     = "environment-template"
+}
+
+variable "ingress_nginx_chart_name" {
+  description = "Ingress-Nginx Helm chart folder name"
+  type        = string
+  default     = "ingress-nginx"
+}
+
+variable "keycloak_chart_name" {
+  description = "Keycloak Helm chart folder name"
+  type        = string
+  default     = "keycloak"
+}
+
+variable "keycloak_realm" {
+  description = "Keycloak realm name"
+  type        = string
+  default     = "infra"
+}
+
+variable "kube_prometheus_stack_chart_name" {
+  description = "Kube Prometheus Stack Helm chart folder name"
+  type        = string
+  default     = "kube-prometheus-stack"
+}
+
+variable "kubeconfig_context" {
+  description = "Kubernetes context to use"
+  type        = string
+}
+
+variable "kubeconfig_path" {
+  description = "Path to the Kubernetes config file"
+  type        = string
+}
+
+variable "oauth2_proxy_cache_chart_name" {
+  description = "OAuth2 Proxy Cache Helm chart folder name"
+  type        = string
+  default     = "oauth2-proxy-cache"
+}
+
+variable "postgresql_chart_name" {
+  description = "PostgreSQL Helm chart folder name"
+  type        = string
+  default     = "postgresql"
+}
+
+variable "prometheus_keycloak_client_id" {
+  description = "Keycloak client ID used assigned to Prometheus"
+  type        = string
+  default     = "prometheus"
+}
+
+variable "prometheus_oauth2_proxy_chart_name" {
+  description = "Prometheus OAuth2 Proxy Helm chart folder name"
+  type        = string
+  default     = "prometheus-oauth2-proxy"
+}
+
+variable "selfsigned_chart_name" {
+  description = "Self-Signed Issuer Helm chart folder name"
+  type        = string
+  default     = "self-signed-issuer"
+}
+
+variable "templates_path" {
+  description = "Path to the templates directory"
+  type        = string
+  default     = "../templates"
+}
+
+variable "valkey_chart_name" {
+  description = "Valkey Helm chart folder name"
+  type        = string
+  default     = "valkey"
+}
+
+variable "webex_access_token" {
+  description = "The Webex access token for the Alertmanager integration"
+  type        = string
+  sensitive   = true
+  default     = ""
 }
