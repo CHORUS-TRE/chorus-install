8b0530ea20b8ce00239f02701d1d7b8ac2bc3e09
Jonathan Haab <jonathan.haab@chuv.ch>
2026-04-30 11:57:36 +0200

    fix var

--- modules/chorus_gateway/main.tf
+++ modules/chorus_gateway/main.tf
@@ -5,13 +5,7 @@ module "chorus_gateway" {
   source = "../chorus_gateway_secret"
 
   gateway_namespace = var.gateway_namespace
-
-  oidc_client_secrets = {
-    "prometheus-oidc-secret"        = module.keycloak_secret.prometheus_client_secret
-    "alertmanager-oidc-secret"      = module.keycloak_secret.alertmanager_client_secret
-    "juicefs-dashboard-oidc-secret" = module.keycloak_secret.juicefs_dashboard_client_secret
-  }
-
+  oidc_client_secrets = var.oidc_client_secrets
 }
 
 # Deploy Chorus Gateway Helm chart
